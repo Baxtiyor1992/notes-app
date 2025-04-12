9c8e0b334ac92f2aebf4f5ecd5a8a2eb36e5186d
MuhammadYasin <muhammadyasinsharifboyev@gmail.com>
2025-04-12 07:11:07 +0500

    libs set successfully

--- NotesApp/app/build.gradle.kts
+++ NotesApp/app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.google.devtools.ksp")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -40,6 +43,49 @@ android {
 }
 
 dependencies {
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.8.9")
+
+    //System bar color controller
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
+
+    //Loading anim
+    implementation("com.github.MahboubehSeyedpour:jetpack-loading:1.1.0")
+
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.56")
+    ksp("com.google.dagger:hilt-android-compiler:2.56")
+    ksp("androidx.hilt:hilt-compiler:1.2.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    //Secure Preference
+    implementation("com.scottyab:secure-preferences-lib:0.1.7")
+
+    //Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    //Live Data
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.8")
+
+    //Snackbar
+    implementation("com.github.stevdza-san:MessageBarCompose:1.0.8")
+
+    //Lottie Anim
+    implementation("com.airbnb.android:lottie-compose:6.4.0")
+
+    //Coil
+    implementation("io.coil-kt.coil3:coil-compose:3.1.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
+
+    //WorkManager
+    implementation ("androidx.hilt:hilt-work:1.2.0")
+    ksp("androidx.hilt:hilt-compiler:1.2.0")
+    implementation( "androidx.work:work-runtime-ktx:2.9.0")
+
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- NotesApp/build.gradle.kts
+++ NotesApp/build.gradle.kts
@@ -1,6 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:8.9.1")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.50")
+        classpath ("org.jetbrains.kotlin:kotlin-serialization:1.9.0")
+    }
+}
+
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
 }
